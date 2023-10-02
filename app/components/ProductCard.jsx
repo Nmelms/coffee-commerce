8a61b87b0d57a66dd1f351e31ecc7ba5ae2dfcde
Nick Melms <nick@craftedagency.com>
2023-10-01 20:51:19 -0400

    added option to change roast bg color based on roast type

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import { Button } from "react-bootstrap";
 
 const ProductCard = ({ name, img, price, roast }) => {
-  console.log(img);
+  let roastStr = roast;
   return (
     <div className="col-4">
-      <div className=" d-flex m-2 border product-card">
+      <div className={`${roast} d-flex m-2 border product-card `}>
         <div className="row h-50">
           <Image
             className="product-img"
@@ -18,7 +18,9 @@ const ProductCard = ({ name, img, price, roast }) => {
         <div className="row d-flex flex-column align-items-center">
           <p>{name}</p>
           <span className="price text-center">${price}</span>
-          <span>{roast}</span>
+          <span className="roast rounded-pill text-center m-3 p-1">
+            {roastStr.charAt(0).toUpperCase() + roastStr.slice(1)}
+          </span>
         </div>
 
         <Button>ADD TO CART</Button>
