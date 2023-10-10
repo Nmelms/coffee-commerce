f208e9c38475c2204729effedd8994bc2a5c0032
Nick Melms <nick@craftedagency.com>
2023-10-09 21:48:23 -0400

    basic styling and info for mobile product page

--- app/store/[id]/page.jsx
+++ app/store/[id]/page.jsx
@@ -1,7 +1,8 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import Image from "next/image";
+import { Button } from "react-bootstrap";
 
 const fetchProduct = async ({ id }) => {
-  console.log(id);
   const api = new WooCommerceRestApi({
     url: "http://ecomm.local",
     consumerKey: process.env.CONSUMER_KEY,
@@ -11,15 +12,32 @@ const fetchProduct = async ({ id }) => {
 
   try {
     const response = await api.get(`products/${id}`);
-
     return response;
   } catch (error) {}
 };
 
 const ProductPage = async ({ params }) => {
-  console.log(params.id);
   const product = await fetchProduct(params);
-  return <div>{product.data.name}</div>;
+  console.log();
+
+  return (
+    <div className="productPage d-flex flex-column  align-items-center space-apart">
+      <Image
+        src={product.data.images[0].src}
+        height={200}
+        width={200}
+        alt={product.data.name}
+      />
+      <div className="productDetails d-flex flex-column justify-content-between">
+        <h2 className="text-white">{product.data.name}</h2>
+        <span>star rating</span>
+        {product.data.description}
+        <span className="productPrice">${product.data.price}</span>
+        <span>Quantity component</span>
+        <Button className="rounded-pill m-3">Order Now</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
