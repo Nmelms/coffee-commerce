56383c6df7ae593dabf9d524b145b4b301d5a7ab
Nick Melms <nick@craftedagency.com>
2023-10-17 21:35:17 -0400

    add cart total

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -10,14 +10,15 @@ import CartCard from "../components/CartCard";
 
 let token = localStorage.getItem("carttoken");
 const cart = () => {
+  const [data, setData] = useState({});
   const [orderData, setOrderData] = useState({ line_items: [], billing: {} });
   const [cartItems, setCartItems] = useState([]);
 
   const priceInDollars = (price) => price / 100;
 
   useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+    console.log(data.totals?.total_price);
+  }, [data]);
 
   useEffect(() => {
     fetch("http://ecomm.local/wp-json/wc/store/v1/cart/", {
@@ -31,6 +32,7 @@ const cart = () => {
       .then((data) => {
         let lineArr = [];
         let cartArr = [];
+        setData(data);
         data.items.map((item) => {
           lineArr.push({ product_id: item.id, quantity: item.quantity });
           cartArr.push(item);
@@ -44,8 +46,8 @@ const cart = () => {
   }, []);
 
   return (
-    <div className="cart">
-      <div className="cart-list container">
+    <div className="cart container  ">
+      <div className="cart-list p-0 container">
         {cartItems?.map((item) => (
           <CartCard
             images={item.images}
@@ -57,7 +59,13 @@ const cart = () => {
           />
         ))}
       </div>
-      <Button>Log products</Button>
+
+      <div className="d-flex justify-content-end">
+        <span className="order-total">
+          Total: {priceInDollars(data.totals?.total_price)}
+        </span>
+      </div>
+
       <AddressForm orderData={orderData} setOrderData={setOrderData} />
       <CheckoutBtn orderData={orderData}>Proceed to Checout</CheckoutBtn>
     </div>
