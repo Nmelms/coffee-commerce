dc305a5cfcd85c8dc992c7bb3073197fe06465e3
Nick Melms <nick@craftedagency.com>
2023-11-10 15:31:13 -0500

    display and update total price in cart

--- app/components/CartCard.jsx
+++ app/components/CartCard.jsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { useState } from "react";
 import RemoveBtn from "./RemoveBtn";
 import Image from "next/image";
 import { Container, Row, Col } from "react-bootstrap";
@@ -6,6 +8,12 @@ import QuantityCounter from "../components/QuantityCounter";
 
 const CartCard = ({ item, setCartItems }) => {
   const priceInDollars = (price) => price / 100;
+  const [totalPrice, setTotalPrice] = useState(
+    priceInDollars(item.prices.price)
+  );
+  const [quantity, setQuantity] = useState(item.quantity);
+  console.log(quantity, "in quantity");
+
   return (
     <Container>
       <Row className="cart-card d-flex ">
@@ -21,7 +29,7 @@ const CartCard = ({ item, setCartItems }) => {
         </Col>
         <Col
           xs={3}
-          lg={5}
+          lg={4}
           className="card-name-wrapper d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center"
         >
           <h4 className="cart-card-name">{item.name}</h4>
@@ -29,15 +37,23 @@ const CartCard = ({ item, setCartItems }) => {
           <QuantityCounter
             setCartItems={setCartItems}
             id={item.key}
-            quantity={item.quantity}
+            quantity={quantity}
+            setQuantity={setQuantity}
           />
         </Col>
         <Col
           xs={3}
-          lg={2}
-          className="d-flex justify-content-center align-items-center"
+          lg={3}
+          className="d-flex justify-content-center align-items-center "
         >
-          ${priceInDollars(item.prices.price)}
+          <div className="p-2 d-flex flex-column text-center">
+            <p>Unit Price</p>
+            <span>${priceInDollars(item.prices.price)}</span>
+          </div>
+          <div className="p-2 d-flex flex-column text-center">
+            <p>total Price</p>
+            <span>${quantity * totalPrice}</span>
+          </div>
         </Col>
         <Col
           xs={3}
--- app/components/QuantityCounter.jsx
+++ app/components/QuantityCounter.jsx
@@ -1,31 +1,40 @@
 "use client";
 import { NextResponse } from "next/server";
+import { debounce } from "lodash";
 import { Button } from "react-bootstrap";
-import { useState, useEffect } from "react";
-const QuantityCounter = ({ quantity, id, setCartItems }) => {
-  const [num, setNum] = useState(quantity);
-  const handleIncrement = async () => {
-    setNum(num + 1);
-    let res = await fetch("http://localhost:3000/api/cart/update", {
-      method: "POST",
-      cache: "no-cache",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ key: id, quantity: num }),
-    });
-    let json = await res.json();
-    if (res.ok) {
-      setCartItems(json.items);
-      return NextResponse.json(json);
-    } else {
-      return NextResponse.json({ message: "error" });
-    }
+import { useState, useEffect, useRef } from "react";
+const QuantityCounter = ({ quantity, setQuantity, id, setCartItems }) => {
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
   };
+
+  const handleDecrement = () => {
+    setQuantity(quantity - 1);
+  };
+
+  // const updateQuantity = async (newNum) => {
+  //   let res = await fetch("http://localhost:3000/api/cart/update", {
+  //     method: "POST",
+  //     cache: "no-cache",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ key: id, quantity: newNum }),
+  //   });
+  //   let json = await res.json();
+  //   if (res.ok) {
+  //     setCartItems(json.items);
+  //     return NextResponse.json(json);
+  //   } else {
+  //     return NextResponse.json({ message: "error" });
+  //   }
+  // };
+
+  // const debouncedUpdate = debounce(updateCart, 300);
   return (
     <div classname="d-flex">
-      {/* <Button onClick={handleDecrement}> - </Button> */}
-      <span className="p-3">{num} </span>
+      <Button onClick={() => handleDecrement()}> - </Button>
+      <span className="p-3">{quantity} </span>
       <Button onClick={() => handleIncrement()}> + </Button>
     </div>
   );
