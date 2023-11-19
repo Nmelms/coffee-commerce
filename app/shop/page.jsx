5a65704190f84ccfd03869a2f9881ed02411e9f0
Nick Melms <nick@craftedagency.com>
2023-11-19 15:06:11 -0500

    uncomment store fetch

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -3,15 +3,15 @@ import ProductList from "../components/ProductList";
 import MobileFilter from "../components/MobileFilter";
 
 const storePage = async () => {
-  // const response = await fetch(
-  //   `${process.env.NEXT_PUBLIC_FRONT_URL}/api/products`,
-  //   {
-  //     method: "GET",
-  //     cache: "no-cache",
-  //   }
-  // );
-  // const products = await response.json();
-  // console.log(products);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_FRONT_URL}/api/products`,
+    {
+      method: "GET",
+      cache: "no-cache",
+    }
+  );
+  const products = await response.json();
+  console.log(products);
 
   return (
     <div className="store">
@@ -22,7 +22,7 @@ const storePage = async () => {
             <MobileFilter data={proudctData} /> */}
           </div>
           <div className="col-12 col-md-9 flex-wrap container border d-flex  ">
-            {/* <ProductList products={products}></ProductList> */}
+            <ProductList products={products}></ProductList>
           </div>
         </div>
       </div>
