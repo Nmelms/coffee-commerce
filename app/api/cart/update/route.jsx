f19676e0f869edd4f025e55d324bae0ec7bc0d11
Nick Melms <nick@craftedagency.com>
2023-11-20 15:35:30 -0500

    update cart/update to use env var

--- app/api/cart/update/route.jsx
+++ app/api/cart/update/route.jsx
@@ -5,20 +5,18 @@ export async function POST(request) {
   let req = await request.json();
   let key = req.key;
   let quantity = req.quantity;
+  let apiURL = process.env.API_URL;
   try {
-    let res = await fetch(
-      ` http://ecomm.local/wp-json/wc/store/v1/cart/update-item`,
-      {
-        method: "POST",
-        cache: "no-store",
-        headers: {
-          "Content-Type": "application/json",
-          Nonce: cookies().get("nonce").value,
-          "Cart-Token": cookies().get("cart-token").value,
-        },
-        body: JSON.stringify({ key, quantity }),
-      }
-    );
+    let res = await fetch(`${apiURL}/wp-json/wc/store/v1/cart`, {
+      method: "POST",
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        Nonce: cookies().get("nonce").value,
+        "Cart-Token": cookies().get("cart-token").value,
+      },
+      body: JSON.stringify({ key, quantity }),
+    });
     let json = await res.json();
 
     return NextResponse.json(json);
