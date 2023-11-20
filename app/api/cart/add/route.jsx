ba49212b48bf7ad1f6a4c1ffd8224fae37bc709c
Nick Melms <nick@craftedagency.com>
2023-11-20 14:21:55 -0500

    update cart/add request to use env variable

--- app/api/cart/add/route.jsx
+++ app/api/cart/add/route.jsx
@@ -2,21 +2,19 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function POST(request) {
+  apiURL = process.env.API_URL;
   let req = await request.json();
   let id = req.id;
   try {
-    let res = await fetch(
-      ` http://ecomm.local/wp-json/wc/store/v1/cart/add-item`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Nonce: cookies().get("nonce").value,
-          "Cart-Token": cookies().get("cart-token").value,
-        },
-        body: JSON.stringify({ id }),
-      }
-    );
+    let res = await fetch(` ${apiURL}/wp-json/wc/store/v1/cart/add-item`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Nonce: cookies().get("nonce").value,
+        "Cart-Token": cookies().get("cart-token").value,
+      },
+      body: JSON.stringify({ id }),
+    });
     let json = await res.json();
 
     return NextResponse.json(json);
