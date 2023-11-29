9e91de1e0113f4380ffd3abd14dce663b3c742df
Nick Melms <nick@craftedagency.com>
2023-11-29 13:44:49 -0500

    update homecard css

--- app/components/HomeCard.jsx
+++ app/components/HomeCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 const HomeCard = ({ img, title, text }) => {
   return (
-    <div className="home-card-wrapper my-3 col-12 col-md-6 col-lg-4 d-flex  flex-column text-center justify-content-center ">
+    <div className="home-card-wrapper my-3 col-12 col-md-6 col-lg-4 text-center">
       <div className="home-card h-100  p-4">
         <Image
           className="home-card-img"
