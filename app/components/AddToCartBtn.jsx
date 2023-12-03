8075864a50455ec7d3e36c48227eb2c281ceb87c
Nick Melms <nick@craftedagency.com>
2023-12-02 21:23:36 -0500

    update cart item quanity if exists in cart

--- app/components/AddToCartBtn.jsx
+++ app/components/AddToCartBtn.jsx
@@ -1,35 +1,81 @@
 "use client";
 import { NextResponse } from "next/server";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import useCartStore from "../useCartStore";
 let carttoken = "";
 
 const AddToCartBtn = ({ product }) => {
   const [cartText, setCartText] = useState("Add To Cart");
-  const handleClick = async (e, id) => {
-    e.preventDefault();
-    let res = await fetch(`api/cart/add/`, {
-      method: "POST",
+  const [cart, setCart] = useState([]);
+
+  const changeText = () => {
+    setCartText("Success");
+    setTimeout(() => {
+      setCartText("Add To Cart");
+    }, 700);
+  };
+
+  useEffect(() => {
+    fetch(`/api/cart`, {
+      method: "GET",
       cache: "no-cache",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ id }),
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCart(data.items);
+      });
+  }, []);
 
-    const changeText = () => {
-      setCartText("Success");
-      setTimeout(() => {
-        setCartText("Add To Cart");
-      }, 700);
-    };
-    if (res.ok) {
-      changeText();
+  const handleClick = async (e, id) => {
+    e.preventDefault();
+    const itemIndex = cart.findIndex((item) => item.id === id);
 
-      return NextResponse.json({ message: "did it" });
+    if (itemIndex !== -1) {
+      const res = await fetch(`api/cart/update/`, {
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          key: cart[itemIndex].key,
+          quantity: cart[itemIndex].quantity + 1,
+        }),
+      });
+      if (res.ok) {
+        let updatedCart = [...cart];
+        updatedCart[itemIndex] = {
+          ...updatedCart[itemIndex],
+          quantity: updatedCart[itemIndex].quantity + 1,
+        };
+        setCart(updatedCart);
+        changeText();
+        return NextResponse.json(res);
+      } else {
+        return NextResponse.json(res);
+      }
     } else {
-      return NextResponse.json({ message: "nope" });
+      // Item does not exist in cart, add it
+      const res = await fetch(`api/cart/add/`, {
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      });
+
+      if (res.ok) {
+        changeText();
+        // Handle response appropriately here
+        return NextResponse.json(res);
+      } else {
+        return NextResponse.json(res);
+      }
     }
   };
 
