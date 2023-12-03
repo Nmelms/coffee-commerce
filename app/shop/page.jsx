1bbb57e0623968a09be7c5ec635ee19f05e5be9b
Nick Melms <nick@craftedagency.com>
2023-12-02 19:55:24 -0500

    remove space for filter on product list

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -27,11 +27,12 @@ const StorePage = () => {
       <div className="container">
         <div className="row">
           <h2 className="d-flex justify-content-center my-5">OUR COFFEE</h2>
-          <div className="col-md-3 position-relative">
-            {/* <SideNav data={proudctData} />
-            <MobileFilter data={proudctData} /> */}
-          </div>
-          <div className="col-12 col-md-9 flex-wrap container border d-flex  ">
+          {/* for the future filter feature */}
+          {/* <div className="col-md-3 position-relative">
+            <SideNav data={proudctData} />
+            <MobileFilter data={proudctData} />
+          </div> */}
+          <div className="col-12 flex-wrap container border d-flex  ">
             <ProductList products={products}></ProductList>
           </div>
         </div>
