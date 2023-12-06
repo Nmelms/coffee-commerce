6e9f54045a833ed984008c059afa7c0116274989
Nick Melms <nick@craftedagency.com>
2023-12-06 13:44:30 -0500

    styled section 3

--- app/components/Section3Card.jsx
+++ app/components/Section3Card.jsx
@@ -3,10 +3,17 @@ import Image from "next/image";
 
 const Section3Card = ({ src, title, text }) => {
   return (
-    <div className="d-flex flex-column align-items-center text-center my-4">
-      <Image height={100} width={100} src={src}></Image>
-      <span className="section3-card-title my-2">{title}</span>
-      <span>{text}</span>
+    <div className="d-flex flex-column row flex-md-row align-items-center text-center my-4">
+      <Image
+        className="col-md-4 section3-card-img"
+        height={100}
+        width={100}
+        src={src}
+      ></Image>
+      <div className="col-md-8 d-flex flex-column text-">
+        <span className="section3-card-title my-2">{title}</span>
+        <span>{text}</span>
+      </div>
     </div>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -40,48 +40,54 @@ export default function Home() {
           </span>
         </div>
       </section>
-      <section className="container-fluid home-section3 d-flex  flex-column ">
-        <Image
-          className="section3-img"
-          src="/worker1.webp"
-          width={500}
-          height={500}
-        ></Image>
-        <span className="store-title mt-4">Online Store</span>
-        <Link className="home-section-store-link" href="/shop">
-          Fresh Coffee, Refresh Memories
-        </Link>
+      <section className="container-fluid home-section3  px-md-4">
+        <div className="row pb-lg-4">
+          <div className="col-lg-6 d-flex flex-column">
+            <Image
+              className="section3-img mx-lg-0 mt-lg-4"
+              src="/worker1.webp"
+              width={500}
+              height={500}
+            ></Image>
+            <span className="store-title mt-4">Online Store</span>
+            <Link className="home-section-store-link" href="/shop">
+              Fresh Coffee, Refresh Memories
+            </Link>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
-          lacus vel facilisis.
-        </p>
-        <Button href="/shop" className="shop-btn rounded-pill">
-          SHOP NOW
-        </Button>
-        <Section3Card
-          src={"/cup1.avif"}
-          title={"Dried Pure Instant Coffee"}
-          text={
-            "Habitant morbi tristict sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          }
-        />
-        <Section3Card
-          src={"/cup2.avif"}
-          title={"Roasted & Grounded Coffee"}
-          text={
-            "Habitant morbi tristict sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          }
-        />
-        <Section3Card
-          src={"/cup3.avif"}
-          title={"Rich Aroma Instant Coffee"}
-          text={
-            "Habitant morbi tristict sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          }
-        />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+              ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
+              accumsan lacus vel facilisis.
+            </p>
+            <Button href="/shop" className="shop-btn rounded-pill">
+              SHOP NOW
+            </Button>
+          </div>
+          <div className="col-lg-6 d-lg-flex flex-lg-column justify-content-lg-between">
+            <Section3Card
+              src={"/cup1.avif"}
+              title={"Dried Pure Instant Coffee"}
+              text={
+                "Habitant morbi tristict sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+              }
+            />
+            <Section3Card
+              src={"/cup2.avif"}
+              title={"Roasted & Grounded Coffee"}
+              text={
+                "Habitant morbi tristict sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+              }
+            />
+            <Section3Card
+              src={"/cup3.avif"}
+              title={"Rich Aroma Instant Coffee"}
+              text={
+                "Habitant morbi tristict sectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+              }
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
