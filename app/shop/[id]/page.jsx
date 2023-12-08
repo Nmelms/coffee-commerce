8f43bc76dee6391d5c097fcfdcea6bc9506be066
Nick Melms <nick@craftedagency.com>
2023-12-08 18:54:22 -0500

    add basic fields to product

--- app/shop/[id]/page.jsx
+++ app/shop/[id]/page.jsx
@@ -2,6 +2,7 @@ import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 import Image from "next/image";
 import { Button } from "react-bootstrap";
 import AddToCartBtn from "../../components/AddToCartBtn";
+import QuantityCounter from "../../components/QuantityCounter";
 import { cookies } from "next/headers";
 
 const fetchProduct = async ({ id }) => {
@@ -19,24 +20,49 @@ const fetchProduct = async ({ id }) => {
 
 const ProductPage = async ({ params }) => {
   const product = await fetchProduct(params);
+  console.log(product.data);
 
   return (
-    <div className="productPage d-flex flex-column  align-items-center space-apart">
-      <Image
-        src={product.data.images[0].src}
-        height={200}
-        width={200}
-        alt={product.data.name}
-      />
-      <div className="productDetails d-flex flex-column justify-content-between">
-        <h2 className="text-white">{product.data.name}</h2>
-        <span>star rating</span>
-        {product.data.description}
-        <span className="productPrice">${product.data.price}</span>
-        <span>Quantity component</span>
-        <AddToCartBtn product={product.data.id} />
+    <>
+      <div className="product-hero w-100 d-flex justify-content-center align-items-center">
+        <h2>{product.data.name}</h2>
       </div>
-    </div>
+      <div className="productPage d-flex flex-column  align-items-start  space-apart mx-5">
+        <Image
+          className="product-img mt-4"
+          src={product.data.images[0].src}
+          height={300}
+          width={300}
+          alt={product.data.name}
+        />
+        <div className="productDetails d-flex  flex-column justify-content-between">
+          <span className="product-name mt-4">{product.data.name}</span>
+          <div className="price-wrapper d-flex">
+            <span className="product-price">Price:</span>
+            <span>{product.data.price}</span>
+          </div>
+          <div className="weight-wrapper d-flex">
+            <span className="product-weight">Weight:</span>
+            <span>{product.data.price}</span>
+          </div>
+          <div className="roast-wrapper d-flex">
+            <span className="product-roast">Roast:</span>
+            <span>{product.data.acf.coffee_roast}</span>
+          </div>
+          <div className="avail-wrapper d-flex">
+            <span className="product-avail">Availibility:</span>
+            <span>{product.data.price}</span>
+          </div>
+          <div className="quantitit-wrapper d-flex">
+            <QuantityCounter />
+          </div>
+
+          <span className="productPrice">${product.data.price}</span>
+          <span>Quantity component</span>
+          <AddToCartBtn product={product.data.id} />
+        </div>
+      </div>
+    </>
   );
 };
 
