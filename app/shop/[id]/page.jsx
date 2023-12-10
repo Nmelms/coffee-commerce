88092a798332f8a3fafcd892ccb18a7acbbc8923
Nick Melms <nick@craftedagency.com>
2023-12-10 14:13:40 -0500

    style product page on all screen sizes

--- app/shop/[id]/page.jsx
+++ app/shop/[id]/page.jsx
@@ -54,57 +54,61 @@ const ProductPage = async ({ params }) => {
       <div className="product-hero w-100 d-flex justify-content-center align-items-center">
         <h2>{product.data.name}</h2>
       </div>
-      <div className="productPage d-flex flex-column  align-items-start  space-apart mx-5">
-        <Image
-          className="product-img mt-4"
-          src={product.data.images[0].src}
-          height={300}
-          width={300}
-          alt={product.data.name}
-        />
-        <div className="productDetails  d-flex  flex-column justify-content-between">
-          <span className="product-name mt-4">{product.data.name}</span>
-          <div className="product-attribute-wrapper d-flex align-items-center  ">
-            <span className="product-attribute-label ">Price:</span>
-            <span className="product-attribute-data price ">
-              ${product.data.price}
+      <div className="productPage container d-flex justify-content-center   space-apart">
+        <div className="row product-row w-100 d-flex mb-lg-5 ">
+          <Image
+            className="product-img col-12 col-lg-6 mt-4"
+            src={product.data.images[0].src}
+            height={300}
+            width={300}
+            alt={product.data.name}
+          />
+          <div className="productDetails col-12  col-lg-6 d-flex  flex-column justify-content-between justify-content-md-around h-75">
+            <span className="product-details-name mt-4">
+              {product.data.name}
             </span>
+            <div className="product-attribute-wrapper d-flex align-items-center  ">
+              <span className="product-attribute-label ">Price:</span>
+              <span className="product-attribute-data price ">
+                ${product.data.price}
+              </span>
+            </div>
+            <div className="product-attribute-wrapper d-flex align-items-center">
+              <span className="product-attribute-label">Weight:</span>
+              <span className="product-attribute-data">
+                {product.data.acf.weight}
+              </span>
+            </div>
+            <div className="product-attribute-wrapper d-flex align-items-center">
+              <span className="product-attribute-label">Roast:</span>
+              <span
+                className={`product-attribute-data ${product.data.acf.coffee_roast}`}
+              >
+                {product.data.acf.coffee_roast}
+              </span>
+            </div>
+            <div className="product-attribute-wrapper d-flex align-items-center">
+              <span className="product-attribute-label">Availibility:</span>
+              <span
+                className={`product-attribute-data ${product.data.stock_status}`}
+              >
+                {product.data.stock_status === "instock"
+                  ? "In Stock!"
+                  : "Sorry! We are out of stock."}
+              </span>
+            </div>
+            <div className="product-attribute-wrapper d-flex align-items-center mb-5">
+              <span className="product-attribute-label">Quantity:</span>
+              <span className="product-attribute-data">
+                <QuantityCounter
+                  id={item[0]?.key}
+                  initialQuan={item[0]?.quantity ? item[0]?.quantity : 0}
+                  productId={product?.data.id}
+                />
+              </span>
+            </div>
+            <AddToCartBtn product={product?.data} />
           </div>
-          <div className="product-attribute-wrapper d-flex align-items-center">
-            <span className="product-attribute-label">Weight:</span>
-            <span className="product-attribute-data">
-              {product.data.acf.weight}
-            </span>
-          </div>
-          <div className="product-attribute-wrapper d-flex align-items-center">
-            <span className="product-attribute-label">Roast:</span>
-            <span
-              className={`product-attribute-data ${product.data.acf.coffee_roast}`}
-            >
-              {product.data.acf.coffee_roast}
-            </span>
-          </div>
-          <div className="product-attribute-wrapper d-flex align-items-center">
-            <span className="product-attribute-label">Availibility:</span>
-            <span
-              className={`product-attribute-data ${product.data.stock_status}`}
-            >
-              {product.data.stock_status === "instock"
-                ? "In Stock!"
-                : "Sorry! We are out of stock."}
-            </span>
-          </div>
-          <div className="product-attribute-wrapper d-flex align-items-center mb-5">
-            <span className="product-attribute-label">Quantity:</span>
-            <span className="product-attribute-data">
-              <QuantityCounter
-                id={item[0]?.key}
-                initialQuan={item[0]?.quantity ? item[0]?.quantity : 0}
-                productId={product?.data.id}
-              />
-            </span>
-          </div>
-          <AddToCartBtn product={product?.data} />
         </div>
       </div>
       <section className="product-description container">
