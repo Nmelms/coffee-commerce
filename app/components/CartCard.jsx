531a4d69e55b511aacac0d855b5d2979da037ec3
Nick Melms <nick@craftedagency.com>
2023-12-11 09:46:50 -0500

    fix alt tag syntax error

--- app/components/CartCard.jsx
+++ app/components/CartCard.jsx
@@ -50,7 +50,7 @@ const CartCard = ({ item, setCartItems, productId }) => {
           <Image
             style={{ objectFit: "contain" }}
             src={item.images[0].src}
-            alt=({})
+            alt="barista tamping coffee"
             className="cart-card-image"
             height={150}
             width={150}
