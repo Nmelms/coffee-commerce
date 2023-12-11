7c2c39055d47df1c339851f0545696a7da779803
Nick Melms <nick@craftedagency.com>
2023-12-11 10:39:14 -0500

    add empty cart icon

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import RemoveBtn from "../components/RemoveBtn";
 import Loading from "../components/Loading";
 import { Button } from "react-bootstrap";
-// import Image from "next/image";
+import Image from "next/image";
 import CheckoutBtn from "../components/CheckoutBtn";
 // import AddressForm from "../components/AddressForm";
 import { Suspense } from "react";
@@ -32,10 +32,22 @@ const Cart = () => {
 
   return (
     <div className="cart container d-flex flex-column  ">
-      <div className="cart-list p-0 ">
+      <div className="cart-list p-0 d-flex justify-content-center align-items-center">
         {cartItems?.map((item) => (
           <CartCard key={item.key} setCartItems={setCartItems} item={item} />
         ))}
+        {cartItems.length === 0 && (
+          <div className="d-flex flex-column text-center">
+            <Image
+              className="empty-cart"
+              alt="empty cart"
+              height={300}
+              width={300}
+              src={"/empty_cart.svg"}
+            />
+            <span className="py-4 cart-empty-text">You Cart is Empty!</span>
+          </div>
+        )}
       </div>
       {data.totals?.total_price && (
         <div className="d-flex justify-content-end">
