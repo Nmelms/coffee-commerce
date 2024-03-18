9e95274cbb9e4a85d89ec1ff960440dfc8d3d994
Nick Melms <nick@craftedagency.com>
2024-03-18 11:32:02 -0400

    remove comments

--- app/api/webhooks/success/route.jsx
+++ app/api/webhooks/success/route.jsx
@@ -7,12 +7,10 @@ export async function POST(res) {
   if (event.type === "payment_intent.succeeded") {
     const paymentIntent = event.data.object;
     const woocommerceId = paymentIntent.metadata.woocommerce_id;
-
-    // Now use this woocommerceId to update the order status in WooCommerce
   }
 
   if (event.type === "checkout.session.completed") {
-    const session = event.data.object; // Checkout Session object
+    const session = event.data.object;
     const woocommerceOrderId = session.metadata.woocommerce_id;
     const WooCommerce = new WooCommerceRestApi({
       url: process.env.API_URL,
@@ -32,8 +30,6 @@ export async function POST(res) {
     } catch (error) {
       return NextResponse.json({ message: "order failed" });
     }
-
-    // Use the woocommerceOrderId to update the corresponding order in WooCommerce
   }
   return NextResponse.json(event);
 }
